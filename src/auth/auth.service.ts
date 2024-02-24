802dd5cfd9b59625546726e015ce36d5959e0680
kurakke <kcanggua@gmail.com>
2024-02-24 22:10:01 +0900

    validateUserメソッド追加

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(
+    username: string,
+    hashedPassword: string,
+  ): Promise<User | null> {
+    const user = await this.userService.findOne(username);
+    if (user && user.hashedPassword === hashedPassword) {
+      return user;
+    }
+    return null;
+  }
+}
