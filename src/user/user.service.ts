f5adbacf761fbad231d56c5eed765c5a4146edc5
kurakke <kcanggua@gmail.com>
2024-02-28 23:14:03 +0900

    userのcreateした

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
+  create(data: CreateUserDto): Promise<User> {
+    return this.prismaService.user.create({ data });
+  }
 
   findOne(name: User['name']): Promise<User> {
     return this.prismaService.user.findUnique({
