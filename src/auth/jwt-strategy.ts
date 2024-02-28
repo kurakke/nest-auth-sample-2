703b89958309590efd9b809a5dc48a26ea8b15e7
kurakke <kcanggua@gmail.com>
2024-02-28 16:23:47 +0900

    jwtpayloadに型つけた

--- src/auth/jwt-strategy.ts
+++ src/auth/jwt-strategy.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from '@prisma/client';
+
+type JwtPayload = {
+  sub: User['id'];
+  username: User['name'];
+};
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +19,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: JwtPayload) {
     return { userId: payload.sub, username: payload.username };
   }
 }
